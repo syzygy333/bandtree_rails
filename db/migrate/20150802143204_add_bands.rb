f3dc4f2d3642200cfaabeb7a1e4ea74e31a05bce
syzygy333 <brianjonashogue@gmail.com>
2015-08-02 10:40:37 -0400

    bands table migration

---
+++ db/migrate/20150802143204_add_bands.rb
@@ -0,0 +1,16 @@
+class AddBands < ActiveRecord::Migration
+  def change
+    create_table :bands do |t|
+      t.string :name, null: false
+      t.string :record_label
+      t.integer :year_formed
+      t.integer :year_disbanded
+      t.integer :reunion_start
+      t.integer :reunion_end
+      t.string :official_link
+      t.string :wiki_link
+      t.text :biography
+      t.boolean :private, default: false
+    end
+  end
+end
