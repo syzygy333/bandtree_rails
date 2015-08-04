ee72ce1bba00543c37c4f5b65a1090a834be060e
syzygy333 <brianjonashogue@gmail.com>
2015-08-04 11:47:19 -0400

    non CW changes to show page, styling

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -10,6 +10,32 @@ class BandsController < ApplicationController
 
   def create
     @band = Band.new(band_params)
+    if current_user == nil
+      flash[:alert] = "You must be signed in to do that."
+      render :new
+    elsif @band.save
+      flash[:success] = "Band added."
+      redirect_to band_path(@band)
+    else
+      flash[:alert] = @band.errors.full_messages.join(".  ")
+      render :new
+    end
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+    if current_user
+      @band.destroy
+      flash[:success] = "Band deleted."
+      redirect_to root_path
+    else
+      flash[:alert] = "You must be signed in to do that."
+      redirect_to band_path(@band)
+    end
   end
 
   private
@@ -18,7 +44,7 @@ class BandsController < ApplicationController
     params.require(:band).permit(
       :name, :record_label, :year_formed, :year_disbanded,
       :reunion_start, :reunion_end, :official_link, :wiki_link,
-      :biography, :private?
+      :biography, :private?, :portrait
     )
   end
 end
